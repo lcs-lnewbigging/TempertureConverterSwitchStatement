81d16fd3814fd00a4fe3bbdbd41c77ceeb6241cd
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-12-03 11:39:28 -0500

    Example of using an if statement that avoids the need for a logical and operator to categorize a value within a range This works because once a condition is matched, all the other branches of the if statement are ignored

--- TempertureConverterSwitchStatement/ContentView.swift
+++ TempertureConverterSwitchStatement/ContentView.swift
@@ -19,19 +19,20 @@ struct ContentView: View {
     }
     //Provide feedback
     var feedback: String{
-        if temperatureInCelsius >= -50.0 && temperatureInCelsius < -20.0 {
+        if temperatureInCelsius < -20.0 {
             return "It's freezing outside! 🥶"
-        } else if temperatureInCelsius >= -20.0 && temperatureInCelsius < 0.0 {
+        } else if temperatureInCelsius < 0.0 {
             return "Dress warmly!"
-        } else if temperatureInCelsius >= 0.0 && temperatureInCelsius < 10.0 {
+        } else if temperatureInCelsius < 10.0 {
             return "Need a coat!"
-        } else if temperatureInCelsius >= 10.0 && temperatureInCelsius < 20.0 {
+        } else if temperatureInCelsius < 20.0 {
             return "Not very cold, not very hot... Perfect!"
-        } else if temperatureInCelsius >= 20.0 && temperatureInCelsius < 30.0 {
+        } else if temperatureInCelsius < 30.0 {
             return "Warm weather! Wear thin layers."
         } else {
             return "Never go out! It's too hot. 🥵"
         }
+        
     }
     
     // "body" property is only for input and output
@@ -41,6 +42,7 @@ struct ContentView: View {
             HStack {
                 Text("Celsius")
                     .bold()
+                    .font(.title2)
                 
                 Spacer()
             }
