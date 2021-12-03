a63e39e7c83aec9edd23f1c5252bade742987347
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-12-03 11:08:59 -0500

    Initial Commit

---
+++ TempertureConverterSwitchStatement/TempertureConverterSwitchStatementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempertureConverterSwitchStatementApp.swift
+//  TempertureConverterSwitchStatement
+//
+//  Created by Luke Newbigging on 2021-12-03.
+//
+
+import SwiftUI
+
+@main
+struct TempertureConverterSwitchStatementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
